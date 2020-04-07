101c17589f67fe1e4d89aa68b15ff242f5be6fdd
fakorede <abiolafakorede@gmail.com>
2020-04-07 20:56:41 +0100

    iota in go

---
+++ iota/README.md
@@ -0,0 +1,3 @@
+# IOTA
+
+> IOTA is a built-in constant generator in Go which generate ever increasing numbers.
---
+++ iota/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		sunday = iota
+		monday
+		tuesday
+		wednesday
+		thursday
+		friday
+		saturday
+	)
+
+	fmt.Println(sunday, monday, tuesday, wednesday, thursday, friday, saturday)
+}
