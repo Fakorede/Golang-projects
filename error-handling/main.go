fa4baedfaf07bd028a60c55777c7e3e561a038bb
fakorede <abiolafakorede@gmail.com>
2020-04-09 22:08:39 +0100

    Error Handling in GO

--- error-handling/main.go
+++ error-handling/main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("The program requires an integer value to run")
+		return
+	}
+
 	age := os.Args[1]
 
 	n, err := strconv.Atoi(age)
