72d67d6477fc15c455ed5c8f700b75830d962ce3
fakorede <abiolafakorede@gmail.com>
2020-04-14 20:24:31 +0100

    retro clock project🕛

---
+++ Retro-Clock/README.md
@@ -0,0 +1,9 @@
+# RETRO-CLOCK
+
+A digital clock that runs in the terminal
+
+### Usage
+
+```
+go run main.go
+```
---
+++ Retro-Clock/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
+
+func main() {
+	type placeholder [5]string
+
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
+	digits := [...]placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+
+	screen.Clear()
+
+	for {
+
+		screen.MoveTopLeft()
+
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+		// fmt.Printf("hour: %d, min: %d, sec:	%d\n", hour, min, sec)
+
+		clock := [...]placeholder{
+			digits[hour/10], digits[hour%10],
+			colon,
+			digits[min/10], digits[min%10],
+			colon,
+			digits[sec/10], digits[sec%10],
+		}
+
+		// for _, digit := range digits {
+		// 	for _, d := range digit {
+		// 		fmt.Printf("%v\n", d)
+		// 	}
+		// 	fmt.Println()
+		// }
+
+		for d := range clock[0] {
+			for index, digit := range clock {
+				next := clock[index][d]
+				if digit == colon && sec%2==0 {
+					next = "   "
+				}
+				fmt.Print(next, " ")
+			}
+
+			fmt.Println()
+		}
+
+		fmt.Println()
+
+		time.Sleep(time.Second)
+
+	}
+
+}
