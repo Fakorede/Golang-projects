bcfdcc2a4e67aa28500dd0e68cca52455f9e051e
fakorede <abiolafakorede@gmail.com>
2020-05-13 23:04:17 +0100

    scanning for input using bufio.Scanner

---
+++ go-bootcamp/log-parser/README.md
@@ -0,0 +1,24 @@
+# Input Scanning
+
+### Bufio.Scanner
+
+Scans an input stream line by line into a buffer
+
+Syntax:
+
+```
+in := bufio.NewScanner(os.Stdin)
+```
+
+#### Methods
+
+in.Scan()
+in.Text()
+in.Bytes()
+in.Err()
+
+Run the program:
+
+```
+go run main.go < proverbs.txt
+```
---
+++ go-bootcamp/log-parser/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// os.Stdin.Close() // closes Standard Input
+	in := bufio.NewScanner(os.Stdin)
+
+	var lines int
+
+	// in.Scan()
+
+	// fmt.Println("Scanned Text :", in.Text())
+	// fmt.Println("Scanned Text :", in.Bytes())
+
+	for in.Scan() {
+		lines++
+		fmt.Println("Scanned Text :", in.Text())
+	}
+
+	fmt.Printf("There are %d line(s)\n", lines)
+
+	if err := in.Err(); err != nil {
+		fmt.Println("ERROR:", err)
+	}
+
+}
